29dd555ca33ed47983e0a3c2777a2c19f545a538
Olli Helenius <liff@iki.fi>
2022-02-28 09:05:07 +0200

    Adapt to new makeDesktopItem

--- pkgs/j-link/default.nix
+++ pkgs/j-link/default.nix
@@ -30,74 +30,74 @@ let
     (makeDesktopItem {
       name = "j-flash";
       desktopName = "SEGGER - J-Flash";
-      categories = "Development;Qt;";
+      categories = [ "Development" "Qt" ];
       exec = "JFlashExe";
       comment = "An application to program data images to the flash of a target device.";
     })
     (makeDesktopItem {
       name = "j-flash-lite";
       desktopName = "SEGGER - J-Flash Lite";
-      categories = "Development;Qt;";
+      categories = [ "Development" "Qt" ];
       exec = "JFlashLiteExe";
       comment = "Flash programming application to program data images to the flash of a target device (lite version for J-Link BASE and EDU).";
     })
     (makeDesktopItem {
       name = "j-flash-spi";
       desktopName = "SEGGER - J-Flash SPI";
-      categories = "Development;Qt;";
+      categories = [ "Development" "Qt" ];
       exec = "JFlashSPIExe";
       comment = "Flash programming application, which allows direct programming of SPI flashes, without any additional hardware.";
     })
     (makeDesktopItem {
       name = "j-link-config";
       desktopName = "SEGGER - J-Link Configurator";
-      categories = "Development;Qt;HardwareSettings;";
+      categories = [ "Development" "Qt" "HardwareSettings" ];
       exec = "JLinkConfigExe";
       comment = "Allows configuration of USB identification as well as TCP/IP identification of J-Link.";
     })
     (makeDesktopItem {
       name = "j-link-gdb-server";
       desktopName = "SEGGER - J-Link GDB Server";
-      categories = "Development;Debugger;Qt;";
+      categories = [ "Development" "Debugger" "Qt" ];
       exec = "JLinkGDBServer";
       comment = "A remote server for GDB making it possible for GDB to connect to and communicate with the target device via J-Link.";
     })
     (makeDesktopItem {
       name = "j-link-license-manager";
       desktopName = "SEGGER - J-Link License Manager";
-      categories = "Development;Qt;";
+      categories = [ "Development" "Qt" ];
       exec = "JLinkLicenseManager";
     })
     (makeDesktopItem {
       name = "j-link-rtt-viewer";
       desktopName = "SEGGER - J-Link RTT Viewer";
-      categories = "Development;Debugger;Monitor;Qt;";
+      categories = [ "Development" "Debugger" "Monitor" "Qt" ];
       exec = "JLinkRTTViewerExe";
     })
     (makeDesktopItem {
       name = "j-link-registration";
       desktopName = "SEGGER - JLink Registration";
-      categories = "Development;Qt;";
+      categories = [ "Development" "Qt" ];
       exec = "JLinkRegistration";
     })
     (makeDesktopItem {
       name = "j-link-remote-server";
       desktopName = "SEGGER - J-Link Remote Server";
-      categories = "Development;Qt;";
+      categories = [ "Development" "Qt" ];
       exec = "JLinkRemoteServer";
       comment = "Utility which provides the possibility to use J-Link / J-Trace remotely via TCP/IP.";
     })
     (makeDesktopItem {
       name = "j-link-swo-viewer";
       desktopName = "SEGGER - J-Link SWO Viewer";
-      categories = "Development;Debugger;Monitor;Qt;";
+      categories = [ "Development" "Debugger" "Monitor" "Qt" ];
       exec = "JLinkSWOViewer";
       comment = "Displays the terminal output of the target using the SWO pin.";
     })
     (makeDesktopItem {
       name = "j-mem";
       desktopName = "SEGGER - J-Mem";
-      categories = "Development;Debugger;Monitor;Qt;";
+      categories = [ "Development" "Debugger" "Monitor" "Qt" ];
       exec = "JMemExe";
       comment = "Application to display and modify the RAM and SFRs (Special Function Registers) of target systems while the target is running.";
     })
